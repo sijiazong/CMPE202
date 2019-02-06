b946ae234b1bf268d0ee78b15b500e928ae17a83
Sijia Zong <sijia.zong@sjsu.edu>
2019-02-06 15:18:46 -0800

    Lab1 Part1.v1
    Updated Gumball class to apply to 3 model. Consider only valid coin values being inserted.

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -3,41 +3,84 @@ public class GumballMachine
 {
 
     private int num_gumballs;
-    private boolean has_quarter;
+    private int cost;
+    private int total_coins;
+    private boolean only_accept_quarter;
 
-    public GumballMachine( int size )
+    public GumballMachine( int size, int cost, boolean only_accept_quarter)
     {
         // initialise instance variables
         this.num_gumballs = size;
-        this.has_quarter = false;
+     //   this.has_quarter = false;
+        this.cost = cost;
+        this.total_coins = 0;
+        this.only_accept_quarter = only_accept_quarter;
     }
 
-    public void insertQuarter(int coin)
+    public void insertCoin(int coin)
     {
-        if ( coin == 25 )
-            this.has_quarter = true ;
+        if (only_accept_quarter && (coin!=25) )
+        {
+            System.out.println("Invalid Coin! Please insert only quarters.");
+        }
+        else
+        {
+            if(isCoinValid(coin))
+            {
+                total_coins = total_coins + coin;
+                System.out.println("You have inserted " + this.total_coins +" cents.");
+            }
+            else
+            {
+                System.out.println("Please insert valid coins.");
+            }
+        }
+    }
+    
+    //Function to test if the coin is valid for the machine.
+    //Valid coins are considered to be value of only coins of the United States dollar in circulation.
+    public boolean isCoinValid(int coin)
+    {
+        if (coin == 1)//Penny
+        return true;
+        if (coin == 5)//Nikel
+        return true;
+        if (coin == 10)//Dime
+        return true;
+        if (coin == 25)//Quarter
+        return true;
+        if (coin == 50)//Half Dollar
+        return true;
         else 
-            this.has_quarter = false ;
+        return false;
+    }
+    
+    public boolean enoughCoin()
+    {
+        if(total_coins == cost)
+            return true;
+        else
+            return false;
     }
     
     public void turnCrank()
     {
-    	if ( this.has_quarter )
-    	{
-    		if ( this.num_gumballs > 0 )
-    		{
-    			this.num_gumballs-- ;
-    			this.has_quarter = false ;
-    			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
-    		}
-    		else
-    		{
-    			System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
-    		}
-    	}
-    	else 
-    	{
-    		System.out.println( "Please insert a quarter" ) ;
-    	}        
+        if ( this.enoughCoin())
+        {
+            if ( this.num_gumballs > 0 )
+            {
+                this.num_gumballs-- ;
+                this.total_coins = 0 ;
+                System.out.println( "Thanks for your coins. Gumball Ejected!" ) ;
+            }
+            else
+            {
+                System.out.println( "No More Gumballs!  Sorry, can't return your coins." ) ;
+            }
+        }
+        else 
+        {
+            System.out.println( "Not enough Coins, please insert more coins." ) ;
+        }        
     }
 }
--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -2,21 +2,29 @@
 
 public class Main {
 
-	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
+    public static void main(String[] args) {
+        //test Gumball Machine 1, 5 gumball, 50 cents cost, only accept Quarters.
+        GumballMachine gumballMachine1 = new GumballMachine(5,50,true);
 
-		System.out.println(gumballMachine);
+        System.out.println("Testing gumball model 2 "+ gumballMachine1);
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
+        gumballMachine1.insertCoin( 25 );
+        gumballMachine1.turnCrank();
+        gumballMachine1.insertCoin( 25 );
+        gumballMachine1.turnCrank();
+        
+        //test Gumball Machine 2, 5 gumball, 50 cents cost, accept all coins.
+        GumballMachine gumballMachine2 = new GumballMachine(5,50,false);
+        System.out.println("Testing gumball model 3 "+ gumballMachine2);
 
-		System.out.println(gumballMachine);
+        gumballMachine2.insertCoin( 10 );
+        gumballMachine2.turnCrank();
+        gumballMachine2.insertCoin( 15 );
+        gumballMachine2.insertCoin( 10 );
+        gumballMachine2.insertCoin( 5 );
+        gumballMachine2.insertCoin( 25 );
+        gumballMachine2.turnCrank();
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
-	}
+        //System.out.println(gumballMachine2);
+    }
 }
