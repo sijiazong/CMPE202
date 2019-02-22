58de1d18de997f30d15fa9dca97c66f90ab93d31
Sijia Zong <sijia.zong@sjsu.edu>
2019-02-21 20:04:27 -0800

    lab3 Sequence Diagram

---
+++ lab3/observer/__SHELL3.java
@@ -0,0 +1,8 @@
+package observer;
+public class __SHELL3 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+
+java.lang.String[] __bluej_param0 = { };
+observer.ObserverTest.main(__bluej_param0);
+
+}}
